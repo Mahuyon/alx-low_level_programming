bd5b89561212f562f3bd6846d8d946e6913fcadd
Mahuyon <latomv@gmail.com>
2022-09-14 20:17:01 -0700

    prints exactly and that piece

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char qut[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2,qut,59);
+	return (1);
+}
