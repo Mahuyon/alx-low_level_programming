43fd51b8ce324eb612577b668722d8ec8fbb0e04
Mahuyon <latomv@gmail.com>
2022-09-22 07:56:36 -0700

    computes abs value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @n: The input number as an integer
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+int _abs(int n)
+{
+	if (n >=0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
