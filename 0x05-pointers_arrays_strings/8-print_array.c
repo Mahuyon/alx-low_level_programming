bd7f80eeaadcc8b6c0b0df882e9452fa22354ed4
Mahuyon <latomv@gmail.com>
2022-09-26 18:03:34 -0700

    updates

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, 
+ * print_array - prints n elements of an array of integers,
  * followed by a new line.
  * @a: input array.
  * @n: input n elements.
