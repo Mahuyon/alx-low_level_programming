b0483fc2b7879ae583004af23135190bd988df2d
Mahuyon <latomv@gmail.com>
2022-09-26 17:40:44 -0700

    generates keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int x = 0, y = 0;
+	time_t gan;
+
+	srand((unsigned int) time(&gan));
+	while (y < 2772)
+	{
+		x = rand() % 128;
+		if ((y + x) > 2772)
+			break;
+		y += r;
+		printf("%c", x);
+	}
+	printf("%c\n", (2772 - y));
+	return (0);
+}
