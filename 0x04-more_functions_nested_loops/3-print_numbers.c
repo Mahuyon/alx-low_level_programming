6e7c6ed56a94e82f45fbdbab05d83e0ae0507934
Mahuyon <latomv@gmail.com>
2022-12-15 17:13:57 -0800

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+
+void print_numbers(void)
+{
+		int pr;
+
+		for (pr = 48; pr < 58; pr++)
+			{
+			_putchar(pr);
+			}
+			_putchar('\n');
+}
+
