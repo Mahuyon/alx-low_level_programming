369f631347cfbdf148a9b729d1b5c26764f7f736
Mahuyon <latomv@gmail.com>
2023-01-10 11:34:35 -0800

    function that detects prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if the input integer is a prime number, otherwise return 0.
+ */
+int is_prime_number(int n)
+{
+
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime_number(n, 2));
+
+}
